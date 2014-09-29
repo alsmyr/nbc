c8897385d24408bd39c398d246217f1d437454cc
alsmyr <erik.alsmyr@accelrys.com>
2014-09-29 10:40:25 +0200

    Responsive price grid

--- NotebookCloud/Controllers/HomeController.cs
+++ NotebookCloud/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using NotebookCloud.Src;
@@ -46,8 +48,25 @@ namespace NotebookCloud.Controllers
             return View();
         }
 
+        public ActionResult MarkettoForm()
+        {
+            var request = WebRequest.Create("http://app.accelrys.com/0358_11m9_global_web_conturtrialTE_eval_requested.html?cmp=0358_11m9_global_web_contur-trial-TE&amp;prd=Contur%20ELN");
+            ((HttpWebRequest)request).UserAgent = Request.UserAgent;
+            
+            var response = request.GetResponse();
+
+            var dataStream = response.GetResponseStream();
+            // Open the stream using a StreamReader for easy access.
+            var reader = new StreamReader(dataStream);
+            // Read the content.
+            var responseFromServer = reader.ReadToEnd();
+
+            return Content(responseFromServer, response.ContentType);
+        }
+
         public ActionResult FreeTrial()
         {
+            
             ViewBag.Message = "Your contact page.";
 
             return View();
